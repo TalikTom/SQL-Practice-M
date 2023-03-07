2bfb133316c72ad971b33ec303420e0c945328cc
EDUKACIJA\student <agic.luke@gmail.com>
2023-03-07 11:55:21 +0100

    Create relations between tables using alter, unify naming across tables

--- LibraryLuka.sql
+++ LibraryLuka.sql
@@ -1,6 +1,6 @@
 use LibraryLuka;
 
-create table member
+create table Member
 (
     id uniqueidentifier PRIMARY KEY not null,
     firstName varchar(50) not null,
@@ -8,38 +8,55 @@ create table member
     oib char(11)
 );
 
-create table membership
+create table Membership
 (
     id uniqueidentifier PRIMARY KEY not null,
     startDate datetime not null,
     endDate datetime,
     accountNo int not null,
-    membership uniqueidentifier not null
+    membershipID uniqueidentifier not null
 );
 
-create table loan
+create table Loan
 (
     id uniqueidentifier PRIMARY KEY not null,
-    startDate datetime not null
+    startDate datetime not null,
+    memberID uniqueidentifier not null
 );
 
-create table book 
+create table Book 
 (
     id uniqueidentifier PRIMARY KEY not null,
     title varchar(255) not null,
-    releaseYear int not null
+    releaseYear int not null,
+    loanID uniqueidentifier not null
 );
 
-create table category 
+create table Category 
 ( 
    id uniqueidentifier PRIMARY KEY not null,
    name varchar(255) not null, 
 );
 
 
-create table book_category
+create table BookCategory
 (
     id uniqueidentifier PRIMARY KEY not null,
-    book_id uniqueidentifier not null,
-    category_id uniqueidentifier not null
+    bookID uniqueidentifier not null,
+    categoryID uniqueidentifier not null
 );
+
+alter table Membership add constraint FK_Membership_Membership_membershipID FOREIGN KEY (membership)
+      REFERENCES Membership (id);
+
+alter table Loan add constraint FK_Loan_Member_memberID FOREIGN KEY (memberID)
+      REFERENCES Member (id);
+
+alter table Book add constraint FK_Book_Loan_loanID FOREIGN KEY (loanID)
+      REFERENCES Loan (id);
+
+alter table BookCategory add constraint FK_BookCategory_Book_bookID FOREIGN KEY (bookID)
+      REFERENCES book (id);   
+
+alter table BookCategory add constraint FK_BookCategory_Category_categoryID FOREIGN KEY (categoryID)
+      REFERENCES category (id);   

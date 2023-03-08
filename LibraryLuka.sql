7e23a6d7544f7905cd5f2238a841345f67cff770
EDUKACIJA\student <agic.luke@gmail.com>
2023-03-08 10:35:45 +0100

    Practice join table basics, inner join

--- LibraryLuka.sql
+++ LibraryLuka.sql
@@ -78,3 +78,16 @@ insert into Book values (newId(), 'The road', '1998', 'A83DEF98-2EE4-42E9-906D-C
 insert into Category values (newid(), 'Horror');
 
 insert into BookCategory values (newid(), 'EE8668F2-EBD9-471C-8AFC-697B7C99F616', '3CE0933F-0788-4BC6-9761-B4117AC224E8');
+
+select a.title, b.CategoryId , c.ReleaseYear, f.AccountNo 
+from Category a
+inner join BookCategory b
+on a.Id = b.CategoryId
+inner join book c
+on c.Id = b.BookId
+inner join loan d 
+on d.Id = c.LoanId
+inner join Member e 
+on e.Id = d.MemberId
+inner join Membership f 
+on f.Id = e.Id;
